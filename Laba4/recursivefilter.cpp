ced3a939a628c715b6f759623d788837c11e8784
istimaldar <istiamldar@gmail.com>
2017-05-09 15:51:42 +0300

    Finaly, it works

---
+++ Laba4/recursivefilter.cpp
@@ -0,0 +1,6 @@
+#include "recursivefilter.h"
+
+RecursiveFilter::RecursiveFilter()
+{
+
+}
---
+++ Laba4/recursivefilter.h
@@ -0,0 +1,11 @@
+#ifndef RECURSIVEFILTER_H
+#define RECURSIVEFILTER_H
+
+
+class RecursiveFilter
+{
+public:
+    RecursiveFilter();
+};
+
+#endif // RECURSIVEFILTER_H
